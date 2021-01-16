4e0bf909100e75d2148ea7cb4552e33cddbbbed2
PixelPickaxe <32438273+PixelPickaxe@users.noreply.github.com>
2021-01-16 15:56:12 -0500

    Add files via upload
    Source files.

---
+++ open_chocolatey_reducer.bat
@@ -0,0 +1,15 @@
+@echo
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.zip
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.7z
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.rar
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.gz
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.tar
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.sfx
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.iso
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.msi
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.msu
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.msp
+del /S %LOCALAPPDATA%\Temp\chocolatey\*.exe
+
+
+
